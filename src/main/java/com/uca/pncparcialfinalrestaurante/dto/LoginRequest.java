df411c2aab67806b7320b842bcc17796ba14870a
cherruca <00402523@uca.edu.sv>
2026-07-08 19:24:00 -0600

    :construction: ADD: DTO for login

---
+++ src/main/java/com/uca/pncparcialfinalrestaurante/dto/LoginRequest.java
@@ -0,0 +1,8 @@
+package com.uca.pncparcialfinalrestaurante.dto;
+
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/uca/pncparcialfinalrestaurante/dto/LoginResponse.java
@@ -0,0 +1,8 @@
+package com.uca.pncparcialfinalrestaurante.dto;
+
+public class LoginResponse {
+
+    private String accessToken;
+    private String refreshToken;
+
+}
